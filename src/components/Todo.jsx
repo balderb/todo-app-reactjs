662f904d97d86e44879464ee8a3c4d9b4fedca5f
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-11-05 16:04:33 +0100

    uupdate editngtemplate's input field with new callback props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -15,7 +15,8 @@ function Todo(props) {
             <label className="todo-label" htmlFor={props.id}>
               New name for {props.name}
             </label>
-            <input id={props.id} className="todo-text" type="text" />
+            <input id={props.id} className="todo-text" type="text" value={newName}
+  onChange={handleChange} />
           </div>
           <div className="btn-group">
             <button type="button" className="btn todo-cancel">
