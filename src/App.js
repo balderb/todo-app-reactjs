fd7f6c1e49b754d718931de9375a294180e1abf0
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-11-05 15:45:29 +0100

    adding editTask to Tasklist in app.jsx

--- src/App.js
+++ src/App.js
@@ -41,6 +41,17 @@ const App = (props) => {
     setTasks(remainingTasks);
   }  
   
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   
 
   function addTask(name) {
@@ -78,6 +89,7 @@ const App = (props) => {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
       />
     )
   );
