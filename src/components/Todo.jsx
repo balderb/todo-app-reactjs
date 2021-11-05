ce1aa8b82aff8fc2984ef838def527c7494d83b3
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-11-05 10:15:52 +0100

    adding props logic inside Todo Component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,21 @@
-function Todo() {
+function Todo(props) {
+    console.log(props)
     return (
         <li className="todo stack-small">
             <div className="c-cb">
                 <input id="todo-0" type="checkbox" defaultChecked={true} />
                 <label className="todo-label" htmlFor="todo-0">
-                    Eat
+                    {props.name}
                 </label>
             </div>
             <div className="btn-group">
             <button type="button" className="btn">
-                 <span className="visually-hidden">Edit</span>
+                Edit
+                 <span className="visually-hidden"></span>
             </button>
             <button type="button" className="btn btn__danger">
-                 <span className="visually-hidden">Delete</span>
+                Delete
+                 <span className="visually-hidden"></span>
             </button>
             </div>
         </li>
